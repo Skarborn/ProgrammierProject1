624aa2e3c152bc740c453bd8a1f0c6ad1a3ac035
Martin Berdau <martinberdau@student.jade-hs.de>
2019-04-21 11:06:46 +0200

    Jetzt auch mit ausfuehrlichem Kommentar

--- test_map.m
+++ test_map.m
@@ -1,7 +1,19 @@
-%% rumprobieren
-% Karte mit Sendemasten finden und gucken, wie man diese bestimmen kann!
+% Dieses Skript dient dazu, die Map-Klasse und das celldata auszuprobieren,
+% um zu gucken, wie man die beiden Elemente verbinden kann. Also fuehlt
+% euch frei, etwas zu ergaenzen und kommentiert genau, welchen Zweck ihr
+% verfolgt. Ganz unten kann man dann generelle Anmerkungen, Erkenntnisse
+% oder Sonstiges schreiben :)
 
-% Daten einlesen mit selben Namen wie in table, damit verstaendlich
+%% Vorgehensweise
+% Die Tabelle "celldata.mat" wird in 1) eingelesen und alle Spalten als Vektor
+% eingespeichert. Anschliessen wird in 2) festgelegt,nach welchen Kriterien
+% wir suchen (Koordinaten, Netzanbieter, ...). Diese Bedingungen werden im
+% Anschluss in 3) genutzt, um die relevanten x- und y-Werte
+% herauszufiltern. Zudem werden die Koordinaten im Anschluss aufgeteilt, so
+% dass Punkte, die Nache am Funkmast sind von fernen getrennt werden. Es
+% folgt die erstellung der Axes-Handle, der Map und ein Plot.
+
+%% 1) Daten einlesen mit selben Namen wie in table, damit verstaendlich
 load('celldata.mat')
 lon = celldata.lon;
 lat = celldata.lat;
@@ -14,7 +26,7 @@ areaCode = celldata.areaCode;
 cellCode = celldata.cellCode;
 distanceInM = celldata.distanceInM;
 
-% Suchkriterien (logcal vectors, die anschliessend eingesetzt werden)
+%% 2) Suchkriterien (logcal vectors, die anschliessend eingesetzt werden)
 
 % Oldenburg coords
 longitudinal_min = 8.18;
@@ -33,11 +45,13 @@ relevant_coords = longitudinal_min <= lon & longitudinal_max >= lon & ...
                 lateral_min <= lat & lateral_max >= lat ;
 relevant_network = (network==criteria_network);
 relevant_networkCode = (networkCode == criteria_networkCode);
+
+% Kombiniere alle Kriterien
 relevant_data =  relevant_coords &...
                 relevant_networkCode &...
                 relevant_network;
 
-% Alle Punkte, die in Oldenburg liegen herausfiltern
+%% 3) Alle Punkte, die in Oldenburg liegen herausfiltern
 x_ol = lon(relevant_data);
 y_ol = lat(relevant_data);
 dist_ol = distanceInM(relevant_data);
@@ -53,15 +67,55 @@ my_coords = struct(...
                     "minLat", lateral_min, ...
                     "maxLat", lateral_max);
 
-%% Map und alle Punkte zeichnen (blau: <=1000m; rot: >1000m)
+%% Map und alle Punkte zeichnen (rot: <=1000m; blau: >1000m)
 
 % Fenster zum Plotten anlegen
 window = figure('Name','Fenster');
-axes_ding = axes(window);
+axes_handle = axes(window);
 
 % map anlegen und plotten
-my_map = Map(my_coords,'osm',axes_ding);
+my_map = Map(my_coords,'osm',axes_handle);
 hold on
-plot(x_ol_nah,y_ol_nah,'b.',x_ol_fern,y_ol_fern,'c.')
+plot(x_ol_nah,y_ol_nah,'r.',x_ol_fern,y_ol_fern,'b.')
 title(['Suchkriterien: Netzwerk: ',criteria_network,...
     ', Netzwerkcode: ',num2str(criteria_networkCode)])
+
+%% Kommentare und Anmerkungen
+
+%% Aufbau eines zukuenftigen Programms
+% Wenn wir das eigentliche Programm schreiben wollen, sollten wir die Werte
+% aus den Tabellen nach bestimmten Kriterien filtern, da die Punkte
+% ansonsten sehr willkuerlich und nichtssagend sind. Bislang wird in diesem
+% Programm nur nach network und networkCode gefiltert. Am besten waere eine
+% GUI in der man diese Angaben machen kann, woraufhin die gewuenschten
+% Punkte eingezeichnet werden
+
+%% created/updated
+% datetime-Objekte. Man koennte Daten nach dem Datum sortieren. Hilft beim
+% erstellen des Grundprogramms eher weniger.
+
+%% network
+% Art des Netzwerks. Notwendiges Filter-Kriterium
+
+%% countryCode
+% Unnoetig, denn alle Punkte liegen in Deutschland.
+
+%% networkCode
+% Laesst auf den Netzanbieter schliessen. 1=Telekom, 2=Vodafone, ...
+% Gutes Filter-Kriterium.
+
+%% areaCode
+% Der areaCode ist fuer jeden Netzanbieter unterschiedlich. Bei allen
+% scheint jedoch der areaCode fuer alle hier behandelten Koordinaten gleich
+% zu sein.
+
+%% cellCode
+% Beim cellCode scheint es sich um einen individuellen Code fuer jedes
+% Handy zu handeln. Wird in erster Linie nicht so nuetzlich fuer uns sein,
+% aber vielleicht kann man das spaeter bei den Extrafunktionen gebrauchen.
+
+%% distance
+% Die Distanzwerte sind ein wenig komisch: alles ueber 1 km wird auf den
+% Meter genau angegeben, unter 1 km wird aber wohl aufgerundet, da 1000 m
+% der Mindestwert ist. Da muessen wir mal gucken, wie wir damit die
+% Position der Sendemasten finden wollen.
